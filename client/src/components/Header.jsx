8f24560c09809ae1264d4d013402eb44af4b4fde
Amit Verma <amit.verma.22080@gmail.com>
2023-10-01 00:26:09 +0530

    basic header structure complete

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,38 @@
 import React from 'react'
-import { AppBar, Toolbar, styled } from '@mui/material'
-import { Menu as MenuIcon } from '@mui/icons-material'
+import { AppBar, Box, InputBase, Toolbar, styled } from '@mui/material'
+import { AccountCircle, AppsOutlined, HelpOutline, Menu as MenuIcon, Search, SettingsOutlined, Tune } from '@mui/icons-material'
 import { gmailLogo} from '../constants/constant'
 
 const StyledAppBar = styled(AppBar)({
     background: '#111111',
-    boxShadow: 'none'
+    boxShadow: 'none' 
 })
+
+const SearchWrapper = styled(Box)({
+    background: '#ffffff',
+    marginLeft: 80,
+    borderRadius: 60,
+    minWidth: 690,
+    maxWidth: 720,
+    height: 48,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: '0 20px',
+    '& > div' : {
+        width: '100%',
+        padding: '0 10px'
+    }
+})
+
+const OptionWrapper = styled(Box)`
+    width: 100%;
+    display: flex;
+    justify-content: end;
+    & > svg {
+        margin-left: 20px;
+    }
+`
 
 const Header = () => {
   return (
@@ -14,6 +40,17 @@ const Header = () => {
         <Toolbar>
             <MenuIcon color='#e6e6e6'/>
             <img src={gmailLogo} alt="logo" style={{marginLeft:15}} />
+            <SearchWrapper>
+                <Search color='action'/>
+                <InputBase placeholder='Search Mail'/>
+                <Tune color='action'/>
+            </SearchWrapper>
+            <OptionWrapper>
+                <HelpOutline color='#e6e6e6'/>
+                <SettingsOutlined color='#e6e6e6'/>
+                <AppsOutlined color='#e6e6e6' />
+                <AccountCircle color='#e6e6e6'/>
+            </OptionWrapper>
         </Toolbar>
     </StyledAppBar>
   )
