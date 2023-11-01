22c2631d4231f18ce13c90987b0aa6b0a56a3d14
Amit Verma <amit.verma.22080@gmail.com>
2023-11-01 22:54:58 +0530

    adding spinner

--- client/src/components/common/SuspenseLoader.jsx
+++ client/src/components/common/SuspenseLoader.jsx
@@ -1,6 +1,12 @@
+import { Box, CircularProgress, Typography } from "@mui/material"
+
+
 const SuspenseLoader = () => {
     return (
-        <p>Loading</p>
+        <Box>
+            <CircularProgress />
+            <Typography>Loading...</Typography>
+        </Box>
     )
 }
 
