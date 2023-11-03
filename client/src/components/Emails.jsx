3f19d54df81a1e8b1825f5ebefd3321d0145ab18
Amit Verma <amit.verma.22080@gmail.com>
2023-11-03 16:51:09 +0530

    added checkbox and delete icon; fix all the colors and border radius on top left

--- client/src/components/Emails.jsx
+++ client/src/components/Emails.jsx
@@ -1,14 +1,33 @@
 import { useOutletContext } from "react-router-dom";
+import { Checkbox, Box, styled } from "@mui/material";
+import { DeleteOutline } from "@mui/icons-material";
+
+const Options = styled(Box)`
+  background-color: #2c2c2c;
+  border-top-left-radius: 15px;
+  padding: 20px 10px 0 10px;
+  display: flex;
+  align-items: center;
+`;
 
 const Emails = () => {
   const { openDrawer } = useOutletContext();
+
   return (
-    <div
-      style={
-        openDrawer ? { marginLeft: 250, width: "100%" } : { width: "100%" }
-      }
-    >
-      Emails
+    <div style={openDrawer ? { marginLeft: 250, borderRadius: 60 } : { width: "100%" }}>
+      <Options>
+        <Checkbox
+          size="small"
+          sx={{
+            color: 'lightgray',
+            '&.Mui-checked': {
+              color: 'lightgray',
+            },
+          }}
+        />
+        <DeleteOutline style={{color: 'lightgray'}} />
+      </Options>
+      <Box></Box>
     </div>
   );
 };
