c2bfc683a084d3f190057dc971c9e00143ef3724
Amit Verma <amit.verma.22080@gmail.com>
2023-11-18 23:09:54 +0530

    email message

---
+++ client/src/components/Email.jsx
@@ -0,0 +1,69 @@
+import { Box, Checkbox, Typography, styled } from "@mui/material";
+import { Star, StarBorder } from "@mui/icons-material";
+import React from 'react'
+
+const Wrapper = styled(Box)`
+    background-color: #161616;
+    color: #b9b9b9;
+    padding: 0 0 0 10px;
+    
+    cursor: pointer;
+
+    display: flex;
+    align-items: center;
+
+    & > div {
+        display: flex;
+        width: 90%;
+        
+
+        & > p {
+            font-size: 14px;
+        }
+    }
+`;
+
+const Date = styled(Typography)`
+    margin-left: auto;
+    font-size: 11px;
+    color: darkgray;
+`;
+
+const Indicator = styled(Typography)`
+    font-size: 12px !important;
+    background-color: #ddd;
+    color: #222;
+    padding: 0 4px;
+    border-radius: 4px;
+    margin-right: 6px;
+`
+
+const Email = ({ email }) => {
+  return (
+    <Wrapper>
+      <Checkbox
+        size="small"
+        sx={{
+          color: "darkgray",
+          "&.Mui-checked": {
+            color: "darkgray",
+          },
+        }}
+      />
+      {/* <Star /> */}
+      <StarBorder fontSize="small" style={{ marginRight: 10 }}/>
+      <Box>
+        <Typography style={{ width: 200, overflow: 'hidden' }}>{email.name}</Typography>
+        <Indicator>Inbox</Indicator>
+        <Typography>{email.subject} {email.body && '-'} {email.body} </Typography>
+        <Date>
+            {(new window.Date(email.date)).getDate()} &nbsp;
+            {(new window.Date(email.date)).toLocaleString('default', { month: 'short' })}
+        </Date>
+      </Box>
+
+    </Wrapper>
+  );
+};
+
+export default Email;
--- client/src/components/Emails.jsx
+++ client/src/components/Emails.jsx
@@ -3,7 +3,8 @@ import { useOutletContext, useParams } from "react-router-dom";
 import { Checkbox, Box, styled, List, ListItem } from "@mui/material";
 import { DeleteOutline } from "@mui/icons-material";
 import { API_URLS } from "../services/api.urls";
-import useApi from '../hooks/useApi'
+import useApi from "../hooks/useApi";
+import Email from "./Email.jsx";
 
 const Options = styled(Box)`
   background-color: #2c2c2c;
@@ -18,37 +19,36 @@ const Emails = () => {
 
   const { type } = useParams();
 
-  const getEmailService = useApi(API_URLS.getEmailFromType)
+  const getEmailService = useApi(API_URLS.getEmailFromType);
 
   useEffect(() => {
-    getEmailService.call({}, type)
-  }, [type])
+    getEmailService.call({}, type);
+  }, [type]);
 
   return (
-    <Box style={openDrawer ? { marginLeft: 250, borderRadius: 60 } : { width: "100%" }}>
+    <Box
+      style={
+        openDrawer ? { marginLeft: 250, borderRadius: 60 } : { width: "100%" }
+      }
+    >
       <Options>
         <Checkbox
           size="small"
           sx={{
-            color: 'lightgray',
-            '&.Mui-checked': {
-              color: 'lightgray',
+            color: "lightgray",
+            "&.Mui-checked": {
+              color: "lightgray",
             },
           }}
         />
-        <DeleteOutline style={{color: 'lightgray'}} />
-
-        {/* list of emails */}
-        <List>
-          {
-            // getEmailService.response
-          }
-        </List>
-
+        <DeleteOutline style={{ color: "lightgray" }} />
       </Options>
-
-      <Box></Box>
-
+      {/* list of emails */}
+      <List>
+        {getEmailService?.response?.map((email) => (
+          <Email key={email._id} email={email} />
+        ))}
+      </List>
     </Box>
   );
 };
