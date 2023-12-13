fd5a6ac807ded19cca6feccc401dafe577d92cc9
Amit Verma <amit.verma.22080@gmail.com>
2023-12-13 19:24:43 +0530

    style subject heading

--- client/src/components/ViewEmail.jsx
+++ client/src/components/ViewEmail.jsx
@@ -3,10 +3,26 @@ import { Box, styled, Typography } from "@mui/material";
 import { ArrowBack, Delete } from "@mui/icons-material";
 import { emptyProfilePic } from "../constants/constant";
 
-const Options = styled(Box)`
-  background-color: #2c2c2c;
-  border-top-left-radius: 15px;
-  padding: 20px 10px 0 10px;
+const IconWrapper = styled(Box)`
+  /* background-color: #2c2c2c; */
+  /* border-top-left-radius: 15px; */
+  padding: 15px;
+`;
+
+const Subject = styled(Typography)`
+  font-size: 22px;
+  margin: 10px 0px 20px 60px;
+  display: flex;
+`;
+
+const Indicator = styled(Box)`
+  font-size: 12px;
+  background-color: #ddd;
+  color: #222;
+  padding: 2px 4px;
+  margin-left: 6px;
+  border-radius: 4px;
+  align-self: center;
 `;
 
 const ViewEmail = () => {
@@ -14,14 +30,14 @@ const ViewEmail = () => {
   const { state } = useLocation();
   const { email } = state;
 
-  const drawerStyle = openDrawer ? { marginLeft: 250, borderRadius: 60, color: "white" } : { width: "100%", color: 'white' };
+  const drawerStyle = openDrawer
+    ? { marginLeft: 250, borderRadius: 60, color: "white" }
+    : { width: "100%", color: "white" };
 
   return (
-    <Box
-      style={drawerStyle}
-    >
+    <Box style={drawerStyle}>
       {/* icons */}
-      <Options>
+      <IconWrapper>
         <ArrowBack
           fontSize="small"
           onClick={() => window.history.back()}
@@ -31,18 +47,35 @@ const ViewEmail = () => {
           fontSize="small"
           style={{ color: "lightgray", cursor: "pointer", marginLeft: 20 }}
         />
-      </Options>
-      
+      </IconWrapper>
+
       {/* Heading/Subject */}
-      <Typography>{email.subject}</Typography>
+      <Subject>
+        {email.subject} 
+        <Indicator component="span">Inbox</Indicator>
+      </Subject>
 
       {/* email body */}
       <Box>
         <img src={emptyProfilePic} alt="" />
-        
+        <Box>
+          <Box>
+            <Typography>
+              {email.name}
+              <Box component="span">&nbsp;&#60;{email.to}&#62;</Box>
+            </Typography>
+            <Box>
+              {new window.Date(email.date).getDate()} &nbsp;
+              {new window.Date(email.date).toLocaleString("default", {
+                month: "long",
+              })}
+              &nbsp;
+              {new window.Date(email.date).getFullYear()}
+            </Box>
+          </Box>
+          <Typography>{email.body}</Typography>
+        </Box>
       </Box>
-        
-      
     </Box>
   );
 };
